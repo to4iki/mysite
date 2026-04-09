3898cc0e0eb7b41d8b8fbbb983677a947636678a
Toshiki Takezawa <tsk.take815@gmail.com>
2026-04-09 22:11:45 +0900

    style: refactor biography section and update item structure for clarity

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@ export const SITE_TITLE = "to4iki.com";
 export const SITE_DESCRIPTION = "website of @to4iki";
 
 export type BiographyItem = {
-  label: string;
+  title: string;
   url: string;
+  description?: string;
 };
